f916e503d84d7064cf58d64f323850dfbc03fb71
saakulkarni <saahas.kulkarni@yestelecom.nl>
2019-05-20 18:46:28 +0530

    Added junit5 lifecycle methods in test class

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,21 +1,39 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GreetingTest {
+	
+	private Greeting greeting;
+	
+	@BeforeAll
+	public static void beforeClass() {
+		System.out.println("Before All -- this is only called once!");
+	}
+	
+	@BeforeEach
+	public void setup() {
+		System.out.println("In before each");
+		 greeting = new Greeting();
+		
+	}
 
 	@Test
 	void testHelloWorld() {
-		Greeting greeting = new Greeting();
-		
 		System.out.println(greeting.helloWorld());
 	}
 
 	@Test
 	void testHelloWorldString() {
-		Greeting greeting = new Greeting();
-		
 		System.out.println(greeting.helloWorld("Saahas"));
+	}
+	
+	@AfterAll
+	public static void afterClass() {
+		System.out.println("After All -- this is only called class");
 	}
 
 }
